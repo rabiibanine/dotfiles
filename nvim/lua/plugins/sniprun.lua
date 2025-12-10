fe4da66c0fa4605863c4bffcbf758071f1113d86
pizzakat <rbanine@gmail.com>
2025-12-11 00:05:41 +0100

    Add keymaps for sniprun

--- nvim/lua/plugins/sniprun.lua
+++ nvim/lua/plugins/sniprun.lua
@@ -5,4 +5,30 @@ return {
   config = function()
     require("sniprun").setup()
   end,
+  keys = {
+    {
+      "<leader>rr",
+      "<cmd>let b:caret=winsaveview() <CR> | <cmd>%SnipRun <CR>| :call winrestview(b:caret) <CR>",
+      mode = { "n" },
+      desc = "Run File",
+    },
+    {
+      "<C-r>",
+      "<cmd>let b:caret=winsaveview() <CR> | <cmd>%SnipRun <CR>| :call winrestview(b:caret) <CR>",
+      mode = { "n" },
+      desc = "Run File",
+    },
+    {
+      "<leader>rc",
+      "<cmd>SnipClose<CR>",
+      mode = { "n" },
+      desc = "Close Output",
+    },
+    {
+      "<C-x>",
+      "<cmd>SnipClose<CR>",
+      mode = { "n" },
+      desc = "Close Output",
+    },
+  },
 }
