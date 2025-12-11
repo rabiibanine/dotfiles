d758884d35bd3ed9848d0d61f303c66a0f42802e
pizzakat <rbanine@gmail.com>
2025-12-11 19:41:13 +0100

    Replace sniprun.nvim with normal keymaps that use snacks.terminal()

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,25 +1,21 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
 local keymap = vim.keymap
 
 keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
-keymap.set("n", "<leader>r", function()
+keymap.set("n", "<C-r>", function()
   local file = vim.fn.expand("%")
+  local file_root = vim.fn.expand("%:r")
   local ft_cmds = {
     python = "python3 " .. file,
     javascript = "node " .. file,
     sh = "bash " .. file,
-    go = "go run " .. file,
-    rust = "cargo run",
+    c = "gcc " .. file .. " -o " .. file_root .. " && " .. file_root,
   }
 
   local cmd = ft_cmds[vim.bo.filetype]
   if cmd then
-    -- Open a terminal and run the command
-    -- Note: 'snacks.terminal' is for newer LazyVim; use 'LazyVim.terminal' for older versions
-    Snacks.terminal(cmd, { win = { position = "bottom" } })
+    -- Open a new terminal and run the current file.
+    Snacks.terminal(cmd, { interactive = true, auto_close = false, win = { position = "bottom" } })
+    print(file)
   else
     print("No run command defined for " .. vim.bo.filetype)
   end
