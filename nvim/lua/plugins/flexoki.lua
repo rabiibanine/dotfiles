121f7bedc65a4bba0ded20f4ab485f56d15b9648
pizzakat <rbanine@gmail.com>
2025-12-11 20:04:16 +0100

    Add flexoki theme to lazyvim.

---
+++ nvim/lua/plugins/flexoki.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "kepano/flexoki-neovim",
+    name = "flexoki",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "flexoki",
+    },
+  },
+}
