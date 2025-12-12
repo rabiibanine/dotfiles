b85f92f6e3278dbf3ac6b8822d2f99973059fa56
pizzakat <rbanine@gmail.com>
2025-12-12 22:57:25 +0100

    Change dashboard header for a thinner one to fix header bug with snacks explorer.

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -1,23 +1,9 @@
 -- 1. Point to the .txt file (Raw ANSI), NOT the .lua file
 -- Make sure this file actually exists at ~/.config/nvim/assets/jojo.txt
-local art_file = vim.fn.stdpath("config") .. "/lua/plugins/assets/jojo.txt"
+local art_file = vim.fn.stdpath("config") .. "/lua/plugins/assets/jojo.lua"
 
 return {
   "folke/snacks.nvim",
-
-  -- 2. The "Nuclear" Fix for wrapping
-  -- This runs an autocommand as soon as the plugin loads to force nowrap
-  init = function()
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "snacks_dashboard",
-      callback = function()
-        vim.schedule(function()
-          vim.opt_local.wrap = false
-          vim.opt_local.spell = false
-        end)
-      end,
-    })
-  end,
 
   opts = {
     dashboard = {
@@ -26,20 +12,17 @@ return {
         {
           section = "terminal",
           cmd = "cat " .. art_file,
-
-          -- Dimensions (Make sure these are huge so it doesn't clip)
-          height = 20,
-          width = 100,
-
+          align = "center",
           padding = 1,
-          indent = -8, -- Manually adjust this number to center it
+          height = 18,
         },
 
+        preset = {},
+
         -- 4. Minimal Buttons
-        { section = "keys", gap = 0, padding = 1 },
-        { section = "startup" },
+        { section = "keys", gap = 0, padding = 0 },
+        { section = "startup", padding = 1 },
       },
     },
   },
 }
-
