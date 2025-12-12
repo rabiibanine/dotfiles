c7bd0c8c4f274ef4dbd7b432ca0593d0d1120b6e
pizzakat <rbanine@gmail.com>
2025-12-12 11:06:24 +0100

    Fix different working directory and execution paths.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,14 +8,16 @@ keymap.set("n", "<A-r>", function()
     python = "python3 " .. file,
     javascript = "node " .. file,
     sh = "bash " .. file,
-    c = "gcc " .. file .. " -o " .. file_root .. " && " .. file_root,
+    c = "gcc " .. file .. " -o " .. file_root .. " && ./" .. file_root,
   }
 
   local cmd = ft_cmds[vim.bo.filetype]
   if cmd then
     -- Open a new terminal and run the current file.
-    Snacks.terminal(cmd, { interactive = true, auto_close = false, win = { position = "bottom" } })
-    print(file)
+    Snacks.terminal(
+      cmd,
+      { interactive = true, auto_close = false, win = { position = "bottom" }, cwd = vim.fn.expand("%:p:h") }
+    )
   else
     print("No run command defined for " .. vim.bo.filetype)
   end
