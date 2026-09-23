87d12077d205b8f94e4f4966b801533f981927ee
rabiibanine <rbanine@gmail.com>
2026-09-23 15:44:53 +0000

    change sign icons

--- nvim/.config/nvim/lua/rabii/plugins/mini.lua
+++ nvim/.config/nvim/lua/rabii/plugins/mini.lua
@@ -17,7 +17,7 @@ return {
 	{
 		"nvim-mini/mini.diff",
 		event = "VeryLazy",
-		opts = { view = { style = "sign" } },
+		opts = { view = { style = "sign", signs = { add = "", change = "", delete = "" } } },
 	},
 	{
 		"nvim-mini/mini.hipatterns",
